6a07a01d77904075e78edfa8b2206a1ed7ae81a0
Georgie-x <georgiex@gmail.com>
2024-05-21 15:49:35 +0100

    aic api working, need to sort format

--- src/components/content-components/Search.tsx
+++ src/components/content-components/Search.tsx
@@ -3,22 +3,27 @@ import axios from "axios"
 import { aicSearch } from "../../utils"
 
 function Search() {
-	const [keyword, setKeyword] = useState<string>("cats")
+	const [keyword, setKeyword] = useState<string>("")
 	const [artList, setArtList] = useState<any[]>([])
 
 	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault()
+		console.log("Form submitted!")
+		console.log(e.currentTarget.keywordSearch.value)
 		setKeyword(e.currentTarget.keywordSearch.value)
 	}
 
 	useEffect(() => {
-		axios.get(aicSearch(keyword)).then(({ data }) => {
-			const artList = data
-			setArtList(artList)
-		})   .catch((error) => {
-            console.error("Error fetching data:", error);
-     
-        });
+		axios
+			.get(aicSearch(keyword))
+			.then(({ data }) => {
+				console.log(data)
+				const artList = data.data
+				setArtList(artList)
+			})
+			.catch((error) => {
+				console.log("Error fetching data:", error)
+			})
 	}, [keyword])
 
 	return (
@@ -30,7 +35,17 @@ function Search() {
 				<button type='submit'>Search</button>
 			</form>
 
-			<div>{artList.length > 0 ? artList : "No art found."}</div>
+			<div>{artList.length > 0 ? `yes, art ${artList.length}` : "No art found."}</div>
+
+			<div id='searchAnswer'>
+				<ul>
+					{artList.map((artwork) => (
+						<li className='answers' key={artwork.id}><a href={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}>link</a>
+							
+						</li>
+					))}
+				</ul>
+			</div>
 		</>
 	)
 }
--- src/utils/AIC-search.tsx
+++ src/utils/AIC-search.tsx
@@ -1,5 +1,5 @@
 function aicSearch(keyword: string): string{
-    return `https://api.artic.edu/api/v1/artworks/search?q=${keyword}&query[term][is_public_domain]=true`
+    return `https://api.artic.edu/api/v1/artworks/search?q=${keyword}&query[term][is_public_domain]=true&fields=id,title,image_id`
 }
 
 export default aicSearch
